45297140e5dfb2bdcf0c54bad18fdd81d229b1ea
RubenJSP <56006571+RubenJSP@users.noreply.github.com>
2021-05-26 18:26:34 -0600

    Circle and Ellipse

--- js/sketch.js
+++ js/sketch.js
@@ -33,6 +33,12 @@ function draw(){
 
           }
     }
+
+    mpCircle(350,(height/2) + 220,radius);
+    text("Middle Point Circle",270,(height/2)+400);
+    mpEllipse(350 + radius*3,(height/2) + 220,radius+50,radius-50)
+    text("Middle Point Ellipse",720,(height/2)+400);
+
     noLoop();
 }
 
@@ -95,3 +101,48 @@ function bresenham(x0, y0, x1, y1) {
         }
     }
  }
+
+ function mpCircle(cX,cY,r) {
+	let x = 0, y = r , p;
+	point(x + cX, y + cY)
+     p = (Number.isInteger(p))? p = 1 - r : p = (5/4) - r;
+	while (x <= y) {
+		if (p < 0) {
+			x = x + 1
+			p = p + (2 * x) + 1
+		} else {
+			x = x + 1
+			y = y - 1
+			p = p + (2 * x) - (2 * y) + 1
+		}
+        point(x + cX, y + cY)
+        point(-x + cX, y + cY)
+        point(x + cX, -y + cY)
+        point(-x + cX, -y + cY)
+        point(y + cX, x + cY)
+        point(-y + cX, x + cY)
+        point(y + cX, -x + cY)
+        point(-y + cX, -x + cY)
+	}
+  }
+
+  function mpEllipse(cX, cY, rX, rY) {
+	let x1 = 0, y1 = rY
+         x2 = rX, y2 = 0;
+	while (Math.pow(rX,2)*y1 >=  Math.pow(rY,2)*x1) {
+		point(x1 + cX, y1 + cY);
+		point(-x1 + cX, y1 + cY);
+		point(x1 + cX, -y1 + cY);
+		point(-x1 + cX, -y1 + cY);
+		x1 = x1 + 1;
+		y1 = (1/rX) * Math.sqrt(Math.pow(rX,2)*Math.pow(rY,2) - Math.pow(rY,2)*Math.pow(x1,2));	
+	}
+	while (Math.pow(rX,2)*y2 <= Math.pow(rY,2)*x2) {
+		point(x2 + cX, y2 + cY);
+		point(-x2 + cX, y2 + cY);
+		point(x2 + cX, -y2 + cY);
+		point(-x2 + cX, -y2 + cY);
+		x2 = (1/rY) * Math.sqrt(Math.pow(rX,2)*Math.pow(rY,2) -Math.pow(rX,2)*Math.pow(y2,2));
+		y2 = y2 + 1;		
+	}
+}
